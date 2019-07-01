9843b8f9eab79fa6404ef857ed3f912dd45fdd11
Tony Huynh <t.hnh@tuta.io>
2019-07-01 11:58:34 +1000

    added form & sanitised user input

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -2,4 +2,19 @@ class FilmsController < ApplicationController
   def index
     @films = Film.all
   end
+
+  def new
+    @film = Film.new
+  end
+
+  def create
+    # @film = Film.new
+    # @film.title = params[:film][:title]
+    # @film.year = params[:film][:year]
+    film_params = params.require(:film).permit(:title, :year)
+    @film = Film.new(film_params)
+    if @film.save
+      redirect_to films_index_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: 'films#index'
   get 'films/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/films", to: "films#index"
+  get "films", to: "films#index"
+  get "films/new", to: "films#new", as: "new_film"
+  post "films", to: "films#create"
 end
